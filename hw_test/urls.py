024462ac9be686c8858b3ddf85c5f3cd6ce150bf
Tchocorpse <Theunknownperson@mail.ru>
2020-06-19 23:07:54 +0300

    Shy step into madness of receiving some info and some styling

--- hw_test/urls.py
+++ hw_test/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = "hw_test"
+
 urlpatterns = [
-    #path('', views.index, name='hw_test'),
+    path('delete_test/<int:delete_id>', views.receive_delete_message, name='hw_test_delete'),
     path('', views.receive_test_message, name='hw_test')
 ]
+
--- hw_test/views.py
+++ hw_test/views.py
@@ -1,25 +1,9 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import HelloTestMessage
 from .forms import TestMessageRecieve
 from django.utils import timezone
-
-
-#def index(request):
-
-    #message_head = HelloTestMessage.objects.all()
-
-   # return render(
-   #     request, "hw_test/index.html", context={"messages_list": message_head,}
-   # )
-    # return HttpResponse("Hello, world and you Цыма. I am test app. Give me your response.")
-
-class TestMessageTemplate():
-    def __init__(self, hello_title, hello_body, publishing_date = 0):
-        self.hello_title = hello_title
-        self.hello_body = hello_body
-        self.publishing_date = publishing_date
-
+from django.urls import reverse
 
 
 def receive_test_message(request):
@@ -29,28 +13,38 @@ def receive_test_message(request):
         if test_form.is_valid():
 
             received_message = HelloTestMessage()
-            received_title = "debug title"
-            received_body = "debug body"
-            #received_message = TestMessageTemplate(received_title, received_body)
-            received_message.hello_title = test_form.cleaned_data['hello_title']
-            received_message.hello_body = test_form.cleaned_data['hello_body']
+
+            received_message.hello_title = test_form.cleaned_data["hello_title"]
+            received_message.hello_body = test_form.cleaned_data["hello_body"]
             received_message.publishing_date = timezone.now()
-            #received_list = [received_message]
+
             received_message.save()
 
-
+            return HttpResponseRedirect("/hw_test/")
+            # return render(
+            #    request, "hw_test/index.html", context={"form": test_form, "messages_list": message_head, }
+            # )
+        else:
 
             return HttpResponseRedirect("/hw_test/")
-            #return render(
-            #    request, "hw_test/index.html", context={"form": test_form, "messages_list": message_head, }
-            #)
-
     else:
         test_form = TestMessageRecieve()
-    message_head = HelloTestMessage.objects.all()
+        message_head = HelloTestMessage.objects.all()
+
+        # return HttpResponseRedirect("/hw_test/")
+        return render(
+            request,
+            "hw_test/index.html",
+            context={"form": test_form, "messages_list": message_head,},
+        )
 
 
-    #return HttpResponseRedirect("/hw_test/")
-    return render(
-        request, "hw_test/index.html", context={"form": test_form, "messages_list": message_head, }
-    )
+def receive_delete_message(request, delete_id):
+    if request.method == "POST":
+        delete_hello = get_object_or_404(HelloTestMessage, pk=delete_id)
+
+        delete_hello.delete()
+        return HttpResponseRedirect("/hw_test/")
+
+    else:
+        return HttpResponseRedirect("/hw_test/")
