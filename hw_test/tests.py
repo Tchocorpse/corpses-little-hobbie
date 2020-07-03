00f200edd32ebe7afa0dc6d2af0d5a9a8c0419c2
Tchocorpse <Theunknownperson@mail.ru>
2020-07-04 00:15:00 +0300

    Playing with templates and another autotest

--- hw_test/tests.py
+++ hw_test/tests.py
@@ -10,12 +10,12 @@ import datetime
 class TrainingTest(TestCase):
     def test_recent_message(self):
         HelloTestMessage.objects.create(
-            hello_title="hello title 1",
+            hello_title="test recent 1",
             hello_body="test body 1",
             publishing_date=timezone.now(),
         )
         HelloTestMessage.objects.create(
-            hello_title="hello title 2",
+            hello_title="test recent 2",
             hello_body="test body 2",
             publishing_date=timezone.now() - datetime.timedelta(days=2),
         )
@@ -25,3 +25,28 @@ class TrainingTest(TestCase):
         resp_hm = response.context["hello_message"]
 
         self.assertEqual(resp_hm, recent_test)
+
+    def test_delete_message(self):
+        HelloTestMessage.objects.create(
+            hello_title="test delete",
+            hello_body="test body delete",
+            publishing_date=timezone.now(),
+        )
+
+        test_message_for_delete = HelloTestMessage.objects.filter(
+            hello_title="test delete"
+        )[0]
+
+        self.client.post(
+            reverse(
+                "hw_test:hw_test_delete",
+                kwargs={"delete_id": test_message_for_delete.id},
+            ),
+            pk=test_message_for_delete.id,
+        )
+
+        deletion_check = HelloTestMessage.objects.filter(
+            hello_title="test delete"
+        ).exists()
+
+        self.assertIs(deletion_check, False)
